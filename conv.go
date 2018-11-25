72c8cbd9f177d5185249426dd694e146152c66b1
SETTER <lphp@mail.ru>
2018-11-25 18:09:45 +0300

    Create package

---
+++ conv.go
@@ -0,0 +1,11 @@
+package tempconv
+
+// FToC и CToF выполняют преобразование температур между двумя различными шкалами;
+// они возвращают отличающиеся от исходных значения
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32) // это преобразование типа Celsius в Fahrenheit
+}
+
+func FToC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9) // это преобразование типа Fahrenheit в Celsius
+}
