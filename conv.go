e97ee48258901f6d6128a4412542598703bd39f0
SETTER <lphp@mail.ru>
2018-11-25 19:06:01 +0300

    Added convert Fahrenheit to Kelvin

--- conv.go
+++ conv.go
@@ -6,10 +6,14 @@ func CToF(c Celsius) Fahrenheit {
 	return Fahrenheit(c*9/5 + 32) // это преобразование типа Celsius в Fahrenheit
 }
 
+func CToK(c Celsius) Kelvin {
+	return Kelvin(c - AbsoluteZeroC) // это преобразование типа Fahrenheit в Celsius
+}
+
 func FToC(f Fahrenheit) Celsius {
 	return Celsius((f - 32) * 5 / 9) // это преобразование типа Fahrenheit в Celsius
 }
 
-func CToK(c Celsius) Kelvin {
-	return Kelvin(c - AbsoluteZeroC) // это преобразование типа Fahrenheit в Celsius
+func FToK(f Fahrenheit) Kelvin {
+	return Kelvin((f − 32) * 5/9 - AbsoluteZeroC) // это преобразование типа Fahrenheit в Celsius
 }
